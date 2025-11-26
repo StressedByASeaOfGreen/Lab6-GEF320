b575a5b1acb46f69c045f2a81c026b3b1bee93a9
StressedByASeaOfGreen <zabderr@outlook.com>
2025-11-26 12:46:01 -0500

    remise prototype

--- competitors/Abderrahmani,Rosa.py
+++ competitors/Abderrahmani,Rosa.py
@@ -3,8 +3,10 @@ from shared import Creature, Propagator, PhotoGland, Direction
 
 class SafeBug(Creature):
     """
-    A minimal creature that never spends strength unless it can afford the cost.
-    Designed not to die immediately.
+    A bug that plays safe, and reproduces and keeps it strength up.
+    this hope that either the enemy is to smart and doesn't attack bugs with higher strength,
+    or that the enemy is dumb and attack us and die.
+    this bug doesn't have any movement so it cant attack. depending on the bug, this can be really disadvantageous
     """
 
     min_baby_cost = Propagator.CREATION_COST + PhotoGland.CREATION_COST + 1
@@ -24,19 +26,16 @@ class SafeBug(Creature):
     def destroyed(cls):
         SafeBug.__count -= 1
 
-    def do_turn(self):
+    def do_turn(self): #make organs, then focus on giving birth
         if not (self.womb and self.leaves < Creature.MAX_ORGANS-1):
             self.make_organ()
 
-
-        # Reproduce only when clearly safe
         else:
-            while self.strength() > (self.min_baby_cost + Propagator.USE_COST)*4:
+            while self.strength() > (self.min_baby_cost + Propagator.USE_COST)*4: #the 4 can be change but >1
                 self.womb.give_birth(self.min_baby_cost, Direction.random())
 
 
-    def make_organ(self):
-        # Build womb if affordable and not already created
+    def make_organ(self): #one womb and all photogland
         if not self.womb and self.strength() > Propagator.CREATION_COST:
             self.womb = SafeProp(self)
 
